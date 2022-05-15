fe2584dd17ee826831212369cde6372e7fc57213
CaioLuppo <luppocode@outlook.com>
2022-05-15 19:15:49 -0300

    Verificando conexão

---
+++ php/validaLogin.php
@@ -0,0 +1,21 @@
+<?php
+// chama o arquivo de conexão
+include('conexao.php');
+
+// verifica se as informações existem (redundância para segurança)
+if(empty($_POST['email']) || empty($_POST['senha'])){
+    header('Location: login.html');
+    exit();
+};
+
+// valida o login, excluindo caracteres que possam originar em SQL Injection
+$email = mysqli_real_escape_string($conexao, $_POST['email']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+
+// guarda a validação
+$query = "SELECT * FROM alunos WHERE email = '{$email}' and senha = MD5('{$senha}');";
+$resultado = mysqli_query($conexao, $query);
+
+echo empty($resultado);
+
+?>
