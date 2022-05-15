26fa530e761cf74d4111f9c1b67b4340f653706f
CaioLuppo <luppocode@outlook.com>
2022-05-15 19:45:58 -0300

    Terminado o validaLogin.php e criado verificaLogin.php

--- php/validaLogin.php
+++ php/validaLogin.php
@@ -1,4 +1,9 @@
 <?php
+// Este arquivo verifica as credenciais e libera o login do usuário
+
+// inicia sessão
+session_start();
+
 // chama o arquivo de conexão
 include('conexao.php');
 
@@ -15,7 +20,16 @@ $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 // guarda a validação
 $query = "SELECT * FROM alunos WHERE email = '{$email}' and senha = MD5('{$senha}');";
 $resultado = mysqli_query($conexao, $query);
+$linhas = mysqli_num_rows($resultado);
 
-echo empty($resultado);
+// valida o login
+if($linhas == 1){
+    $_SESSION['email'] = $email;
+    header('Location: ../meuscursos.php');
+    exit();
+} else {
+    header('Location: ../login.html');
+    exit();
+}
 
 ?>
---
+++ php/verificaLogin.php
@@ -0,0 +1,13 @@
+<?php
+// Este arquivo verifica se o usuário está logado ou não
+
+// incia a sessão
+session_start();
+
+// Se não estiver logado, manda para tela de login
+if(!$_SESSION['email']){
+    header('Location: ../login.html');
+    exit();
+}
+
+?>
