41255103fdc7ba469a106528cb28f25ba7a26f4a
CaioLuppo <luppocode@outlook.com>
2022-05-15 18:49:31 -0300

    CPF e EMAIL setadas como primary key

--- sql/banco_de_dados.sql
+++ sql/banco_de_dados.sql
@@ -6,9 +6,11 @@ USE LOGIN_INFO;
 -- Criação da tabela de alunos:
 
 CREATE TABLE IF NOT EXISTS alunos (
-	cpf char(11) NOT NULL PRIMARY KEY,
+	cpf char(11) NOT NULL,
     nome varchar(40) NOT NULL,
     senha binary(32) NOT NULL,
     plano char(12) NOT NULL,
-    email varchar(32) NOT NULL
+    email varchar(32) NOT NULL,
+
+    PRIMARY KEY(cpf, email)
 );
