e2cb1393b21ff6ad60bfca21f28b6ff90aa17685
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-05-17 20:09:21 -0300

    Corrigido bug por não encontrar o elemento quando se está em outras páginas que não o tem

--- src/js/modules/selection-plano.js
+++ src/js/modules/selection-plano.js
@@ -1,15 +1,14 @@
 export default function initSelectionPlano() {
-  
-}
+  const total = document.querySelector('.total');
+  const plano = document.querySelector('#plano');
 
-const total = document.querySelector('.total');
-const plano = document.querySelector('#plano');
-
-function selectPrice() {
+  function selectPrice() {
+    const elementPrice = document.querySelector(`[value="${this.value}"]`);
+    const price = elementPrice.getAttribute('data-price');
+    total.innerText = `R$${price}`;
+  }
   
-  const elementPrice = document.querySelector(`[value="${this.value}"]`)
-  const price = elementPrice.getAttribute('data-price');
-  total.innerText = `R$${price}`;
-  
+  if (total && plano) {
+    plano.addEventListener('click', selectPrice);
+  }
 }
-plano.addEventListener('click', selectPrice);
