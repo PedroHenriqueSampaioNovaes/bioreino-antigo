793da9061f474b4623c0244f7d5d6d977b6928c1
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-05-20 22:20:34 -0300

    Add script para menu dropdown

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initAlterHeader from './modules/alter-header.js';
 import initSelectionPlano from './modules/selection-plano.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAlterHeader();
 initSelectionPlano();
+initDropdownMenu();
