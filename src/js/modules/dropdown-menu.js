f17c0ca5f1d1cbf7b4edf1b9d6b61757af8cb90b
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-05-20 23:50:10 -0300

    Correção de estilo e de código js incorreto

--- src/js/modules/dropdown-menu.js
+++ src/js/modules/dropdown-menu.js
@@ -1,26 +1,28 @@
 export default function initDropdownMenu() {
+  const navegacaoUsuario = document.querySelector('[data-dropdown="nav"]');
+  const menuDropdown = document.querySelector('[data-dropdown="menu"]');
+  const events = ['click', 'touch'];
   
-}
+  function closeMenu(element, outside) {
+    if (!menuDropdown.contains(element)) {
+      menuDropdown.classList.remove('ativo');
+      navegacaoUsuario.removeAttribute(outside);
+    }
+  }
+  
+  function openMenu(event) {
+    menuDropdown.classList.add('ativo');
+    const outside = 'data-outside';
+  
+    if (navegacaoUsuario.hasAttribute(outside)) {
+      closeMenu(event.target, outside);
+    } else {
+      navegacaoUsuario.setAttribute(outside, '');
+    }
+  }
 
-const navegacaoUsuario = document.querySelector('[data-dropdown="nav"]');
-const menuDropdown = document.querySelector('[data-dropdown="menu"]');
-const events = ['click', 'touch'];
-
-function closeMenu(element, outside) {
-  if (!menuDropdown.contains(element)) {
-    menuDropdown.classList.remove('ativo');
-    navegacaoUsuario.removeAttribute(outside);
+  if (menuDropdown && navegacaoUsuario) {
+    events.forEach((event) => navegacaoUsuario.addEventListener(event, openMenu));
   }
 }
 
-function openMenu(event) {
-  menuDropdown.classList.add('ativo');
-  const outside = 'data-outside';
-
-  if (navegacaoUsuario.hasAttribute(outside)) {
-    closeMenu(event.target, outside);
-  } else {
-    navegacaoUsuario.setAttribute(outside, '');
-  }
-}
-events.forEach((event) => navegacaoUsuario.addEventListener(event, openMenu));
