fa5a888f69539768de4c64cbaa6f25fa34c07106
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-05-24 19:17:36 -0300

    Alteração do código de limitar o tamanho do nome do usuário

--- src/js/modules/username-limited.js
+++ src/js/modules/username-limited.js
@@ -2,11 +2,9 @@ export default function initUserNameLimited() {
   const name = document.querySelector('[data-usuario="name"]');
 
   if (name) {
-    const splitName = name.innerText.split(' ');
-    const [firstName] = splitName;
-    const shortName = firstName.slice(0, 14).trim();
-
-    if (firstName.length >= 15) name.innerText = `${shortName}...`;
+    const shortName = name.innerText.slice(0, 14).trim();
+    
+    if (name.innerText.length >= 15) name.innerText = `${shortName}...`;
     else name.innerText = shortName;
   }
 }
